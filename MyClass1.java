94e3872ba872c44046b35411358a9127013c48b5
Isaac Bustamante <izaack.97@gmail.com>
2020-08-17 13:56:43 -0700

    Thread hijo

--- MyClass1.java
+++ MyClass1.java
@@ -3,19 +3,59 @@ public class MyClass1 {
     
      /**
          Una es extendiendo de la clase Thread y 
+         *  otra desde la interface Runnable() pasandole
+         * uyn thread como parametro a la instancia
+         */
+
+    // public static void main(String[] args) {
+    //     MyClass2 mc = new MyClass2();
+
+    //     // Este metodo hace correr el Thread
+    //     mc.start();
+        
+    //     // Thread en una interface
+    //     MyClass3 mc3 = new MyClass3();
+    //     Thread th = new Thread(mc3);
+    //     th.start();    
+    // }   
+
+    public static void main(String[] args) {
+        
+        /**
+         Una es extendiendo de la clase Thread y 
          * y otra desde la interface Runnable() pasandole
          * un thread como parametro a la instancia
          */
-
-    public static void main(String[] args) {
-        MyClass2 mc = new MyClass2();
-
-        // Este metodo hace correr el Thread
-        mc.start();
         
-        // Thread en una interface
-        MyClass3 mc3 = new MyClass3();
-        Thread th = new Thread(mc3);
-        th.start();
-    }   
+        // TODO code application logic here
+        // MyClass2 mc = new MyClass2();
+        // mc.start();
+        
+        // // Thread en una interface
+        // MyClass3 mc3 = new MyClass3();
+        // Thread th = new Thread(mc3);
+        // th.start();
+
+        Thread t = Thread.currentThread(); // Nos dice que thread se esta ejecutando
+        System.out.println("Thread Actual: \n" + t);
+        t.setName("Principal");
+        System.out.println("Despues del cambio: \n" + t);
+
+        MyClass2 md = new MyClass2();
+        md.start();
+
+        try {
+            System.out.print("Esperando a que finalize el programa");
+
+            for(int i = 0 ; i <=3; i++) {
+                t.sleep(2000);
+                System.out.print(" . ");
+            }
+
+            System.out.println("\nBye! :( ");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
--- MyClass2.java
+++ MyClass2.java
@@ -1,7 +1,13 @@
 public class MyClass2 extends Thread {
+    
+    Thread t;
+
+    public MyClass2() {
+        t = new Thread(this, "Hijo 1");
+    }
+    
     // El Thread se va a ejecutar con el metodo run
     public void run() {
-        System.out-println("Este codigo se ejecuta en un Thread");
-        
+        System.out.println("Hijo" + t);
     } 
 }
